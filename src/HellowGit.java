f7447dbf3b685823e879994398d47501d77377de
Aiarionn <serparkin200996@gmail.com>
2020-07-21 17:56:33 +0300

    Ye asdad ar af af advfafad

--- src/HellowGit.java
+++ src/HellowGit.java
@@ -2,6 +2,6 @@ public class HellowGit {
     public static void main(String[] args) {
         System.out.println("HellowGit!");
         System.out.println("line 2");
-        //SLava ROSSII
+        //SLava ROSSII ad
     }
 }
